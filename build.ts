650ed3e0e729a5e51aa5ced7f8ccc00e4edf6fc5
Pavel Zavadski <zavadskiy.pavel2002@outlook.com>
2026-01-04 22:31:28 +0400

    Clean up build output directory and enhance asset naming conventions

--- build.ts
+++ build.ts
@@ -1,5 +1,6 @@
 import tailwind from "bun-plugin-tailwind"
 
+await Bun.$`rm -r dist`
 await Bun.build({
     plugins: [tailwind],
     outdir: "dist",
@@ -7,5 +8,10 @@ await Bun.build({
     target: "browser",
     sourcemap: "linked",
     minify: true,
+    naming: {
+        entry: "[name].[ext]",
+        chunk: "assets/[name].[hash]",
+        asset: "assets/[name].[hash].[ext]"
+    }
 })
 await Bun.$`cp -r public/* dist/`
